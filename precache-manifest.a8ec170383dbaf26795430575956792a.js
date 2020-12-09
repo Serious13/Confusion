220fd1dd5c02e5d34158722058e7c7d9be760b6f
= <=>
2020-12-09 23:55:44 +0100

    Updates

---
+++ precache-manifest.a8ec170383dbaf26795430575956792a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119e55f536453bc2d2516239da6f66b8",
+    "url": "/Confusion/index.html"
+  },
+  {
+    "revision": "24933dee0b315711c3db",
+    "url": "/Confusion/static/css/2.814665f7.chunk.css"
+  },
+  {
+    "revision": "340f01b5743063d35512",
+    "url": "/Confusion/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "24933dee0b315711c3db",
+    "url": "/Confusion/static/js/2.9ec89d6d.chunk.js"
+  },
+  {
+    "revision": "ddb8cc849c8fa7383819433dd0471bc1",
+    "url": "/Confusion/static/js/2.9ec89d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340f01b5743063d35512",
+    "url": "/Confusion/static/js/main.66e4d05d.chunk.js"
+  },
+  {
+    "revision": "3f69a5781bff6e392284",
+    "url": "/Confusion/static/js/runtime-main.2f180db3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
